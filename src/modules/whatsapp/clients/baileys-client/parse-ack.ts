0052ef2ebb77c1cb116389d01b1a1b19fb176c45
Renan Dutra <r.granatodutra@gmail.com>
2026-02-26 23:57:17 -0300

    refactor: remove unused Baileys client files and processing logger

--- src/modules/whatsapp/clients/baileys-client/parse-ack.ts
+++
@@ -1,9 +0,0 @@
-import { InpulseMessageStatus } from "../../inpulse-types";
-
-const ACKS_DICT: Array<InpulseMessageStatus> = ["ERROR", "PENDING", "SENT", "RECEIVED", "READ", "READ"];
-
-function parseAck(ack: number): InpulseMessageStatus {
-  return ACKS_DICT[ack] || "ERROR";
-}
-
-export default parseAck;
