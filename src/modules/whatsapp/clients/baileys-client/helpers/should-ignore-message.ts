8fd7aabbf25fd78dc30e2d2c0d93ac8143d081bb
leninpkg <leninpkg@gmail.com>
2026-03-01 01:12:24 -0300

    refactor: add message age check to shouldIgnoreMessage function

--- src/modules/whatsapp/clients/baileys-client/helpers/should-ignore-message.ts
+++ src/modules/whatsapp/clients/baileys-client/helpers/should-ignore-message.ts
@@ -1,8 +1,19 @@
+import "dotenv/config";
 import { isJidMetaAI, isJidNewsletter, isJidStatusBroadcast, WAMessage } from "baileys";
 
 interface ShouldIgnoreResult {
   ignore: boolean;
   reason: string;
+}
+
+const SEVEN_DAYS_AGO = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+const HISTORY_MIN_DATE = process.env["HISTORY_MIN_DATE"] ? new Date(process.env["HISTORY_MIN_DATE"]) : SEVEN_DAYS_AGO;
+
+function isMessageTooOld(message: WAMessage): boolean {
+  if (!message.messageTimestamp) return false; // If timestamp is missing, we can't determine age, so we don't ignore based on this criterion
+  const msgDate = new Date(+message.messageTimestamp * 1000);
+
+  return HISTORY_MIN_DATE > msgDate;
 }
 
 export default function shouldIgnoreMessage(message: WAMessage): ShouldIgnoreResult {
@@ -17,6 +28,7 @@ export default function shouldIgnoreMessage(message: WAMessage): ShouldIgnoreRes
   if (message.key.fromMe) return { ignore: true, reason: "Message from self" };
   if (message.message?.reactionMessage) return { ignore: true, reason: "Reaction message" };
   if (message.message?.protocolMessage) return { ignore: true, reason: "Protocol message" };
+  if (isMessageTooOld(message)) return { ignore: true, reason: "Message too old" };
 
   return { ignore: false, reason: "" };
 }
